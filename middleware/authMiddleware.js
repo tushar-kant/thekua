61ed4b047074b98769189042a713ab513f758a39
akashkumarnayak2004 <ece.190410029@silicon.ac.in>
2024-09-02 12:01:06 +0530

    final

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 const authenticate = async (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
     if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
+        return res.status(401).json({ message: 'Unauthorized' });
     }
 
     try {
